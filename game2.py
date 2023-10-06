706fd2792c2accbd8ae5711c49d0752d41a77382
Your Name <you@example.com>
2023-10-06 17:26:29 +0300

    new

--- game2.py
+++ game2.py
@@ -6,63 +6,68 @@ screen_width = 800
 screen_height = 600
 
 display = pg.display.set_mode((screen_width, screen_height))
-display.fill('pink', (0, 0, screen_width, screen_height))
-#pg.display.set_caption('Космическое вторжение')
-#icon_img = pg.image.load('resources/img/ufo.png')
-#pg.display.set_icon(icon_img)
+display.fill('light blue', (0, 0, screen_width, screen_height))
+
 
 background_img = pg.image.load('resources/img/background.png')
 display.blit(background_img, (0, 0))
 
 # шрифт для букв
-sysfont = pg.font.SysFont('arial', 40)
-text_img = sysfont.render('Score: 123', True, 'red')
+sysfont  = pg.font.SysFont('arial', 40)
+text_img = sysfont.render('Ye Min Htwe ', True, 'red')
 
-#font = pg.font.Font('resources/font/04B_19__.TTF', 48)
-#game_over_img = font.render('Game Over', True, 'white')
-w = game_over_img.get_width()
-h = game_over_img.get_height()
+w = text_img.get_width()
+h = text_img.get_height()
 x = screen_width/2 - w/2
-y = screen_height/2 - h/2
-display.blit(game_over_img, (x, y))
+y = screen_height/2 -(h*2)
 
-player_img = pg.imgame.load('resources/img/player.png')
+
+player_img = pg.image.load('resources/img/player.png')
 player_width = player_img.get_width()
-player_height = player_img.get.height()
+player_height = player_img.get_height()
 player_x = screen_width/2 - player_width/2
 player_y = screen_height - player_height
-player_velocity = 1
-Player_dx= 0
 
-bullet_img = pg.imgame.load('resources/img/player.png')
+player_dx= 0
+player_velocity = 1
+
+bullet_img = pg.image.load('resources/img/bullet.png')
 bullet_width = bullet_img.get_width()
-bullet_height = bullet_img.get.height()
-bullet_x = 0
-bullet_y = 0
+bullet_height = bullet_img.get_height()
+bullet_x = player_x
+bullet_y = player_y - bullet_height
 bullet_dy = -2
+bullet_visible = False
 
+def bullet_create():
+    global bullet_y, bullet_x, bullet_visible
+    bullet_x = player_x
+    bullet_y = player_y - bullet_height
+    bullet_visible = True
 
-
-y = screen_height - bullet_height
-player_velocity = 1
-Player_dy = -2
-
-running = True
+running  = True
 while running:
-    # рисовать
-    #display.fill('blue', (20, 50, 100, 250))
+
 
     player_x = player_x + player_dx
     if player_x < 0:
         player_x = 0
-    bullet_y = bullert_y + bullet_dy
-    if player_y < 0:
-        bullet_visible = flase
-        print(f"{bullet_visible=}")
+    if player_x > 750:
+        player_x = 750
+    if player_x + player_width > screen_width:
+        player_x = screen_width - player_width
+
+    if bullet_visible:
+        bullet_y = bullet_y + bullet_dy
+        if bullet_y < 0:
+            bullet_visible = False
+            print(f"Bullet out {bullet_visible}")
 
     display.blit(background_img, (0, 0))
-    display_blit(player_img,(player_x,player_y))
-    display_blit(bullet_img,(player_x,player_y))
+    #display.fill('blue', (0, 0, screen_width, screen_height))
+    display.blit(player_img, (player_x,player_y))
+    if bullet_visible:
+        display.blit(bullet_img, (bullet_x, bullet_y))
     pg.display.update()
 
 
@@ -75,15 +80,20 @@ while running:
             running = False
 
         if event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
-            Player_dx=player_velocity
+            player_dx = - player_velocity
         if event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
-            Player_dx=player_velocity
+            player_dx = player_velocity
 
-        if event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
-            Player_dx= - 0
+        if event.type == pg.KEYUP and event.key == pg.K_LEFT:
+            player_dx = 0
 
-        if event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
-            Player_dx=player_velocity
+        if event.type == pg.KEYUP and event.key == pg.K_RIGHT:
+            player_dx = 0
+
+        # Fire!
+        if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+            print('Fire!')
+            bullet_create()
 
 
 
